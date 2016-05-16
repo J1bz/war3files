463b03934aec914e0715fe302ae7a3c9ca6e4226
Jean-Baptiste Braun <jbaptiste.braun@gmail.com>
2016-05-17 00:17:52 +0200

    First units application implementation : Race and Unit models

--- units/admin.py
+++ units/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from units.models import Race, RaceForm, Unit, UnitForm
 
-# Register your models here.
+
+class RaceAdmin(admin.ModelAdmin):
+    form = RaceForm
+    list_display = ('name',)
+    search_fields = ('name',)
+
+
+class UnitAdmin(admin.ModelAdmin):
+    form = UnitForm
+    list_display = ('name', 'race', 'icon', 'building', 'heroic',)
+    search_fields = ('name', 'race__name',)
+
+admin.site.register(Race, RaceAdmin)
+admin.site.register(Unit, UnitAdmin)
--- units/models.py
+++ units/models.py
@@ -1,3 +1,33 @@
-from django.db import models
+from django.db.models import (
+    Model, CharField, ForeignKey, BooleanField, ImageField)
+from django.forms import ModelForm
 
-# Create your models here.
+
+class Race(Model):
+    name = CharField(max_length=32, primary_key=True)
+
+    def __str__(self):
+        return self.name
+
+
+class RaceForm(ModelForm):
+    class Meta:
+        model = Race
+        fields = ('name',)
+
+
+class Unit(Model):
+    name = CharField(max_length=64)
+    race = ForeignKey(Race)
+    icon = ImageField(upload_to='icons')
+    building = BooleanField(default=False)
+    heroic = BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
+
+class UnitForm(ModelForm):
+    class Meta:
+        model = Unit
+        fields = ('name', 'race', 'icon', 'building', 'heroic',)
