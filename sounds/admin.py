b689a75d42f1f3260b6fcec1844bea898efa38d5
Jean-Baptiste Braun <jbaptiste.braun@gmail.com>
2016-05-22 19:19:08 +0200

    Fix sounds search fields

--- sounds/admin.py
+++ sounds/admin.py
@@ -5,7 +5,7 @@ from sounds.models import Sound, SoundForm
 class SoundAdmin(admin.ModelAdmin):
     form = SoundForm
     list_display = ('name', 'unit', 'audio',)
-    search_fields = ('name', 'unit__name', 'unit__race__name', 'audio__name',)
+    search_fields = ('name', 'unit__name', 'unit__race__name',)
 
 
 admin.site.register(Sound, SoundAdmin)
