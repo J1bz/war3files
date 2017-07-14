64bf0ac7ddc4c8c64c5731da969d29e0dd0367bc
Jean-Baptiste Braun <jbaptiste.braun@gmail.com>
2017-07-15 01:23:39 +0200

    Use OverwriteStorage instead of standard FileSystemStorage leaving unused file behind

--- units/models.py
+++ units/models.py
@@ -1,15 +1,23 @@
-from os.path import basename
+from os import basename
 
 from django.db.models import (
     Model, CharField, ForeignKey, BooleanField, ImageField, FileField,
     SlugField)
 from django.forms import ModelForm
 
+from units.storage import OverwriteStorage
+
 
 class Race(Model):
     name = CharField(max_length=32)
     slug = SlugField(unique=True, max_length=32)
-    icon = ImageField(upload_to='race_icons', null=True, blank=True)
+    icon = ImageField(
+        max_length=128,
+        upload_to='race_icons',
+        storage=OverwriteStorage(),
+        null=True,
+        blank=True
+    )
 
     def __str__(self):
         return self.name
@@ -25,7 +33,13 @@ class Unit(Model):
     name = CharField(max_length=64)
     slug = SlugField(unique=True, max_length=64)
     race = ForeignKey(Race)
-    icon = ImageField(upload_to='icons', null=True, blank=True)
+    icon = ImageField(
+        max_length=128,
+        upload_to='icons',
+        storage=OverwriteStorage(),
+        null=True,
+        blank=True
+    )
     building = BooleanField(default=False)
     heroic = BooleanField(default=False)
     special = BooleanField(default=False)
@@ -54,7 +68,11 @@ class Sound(Model):
     name = CharField(max_length=64)
     slug = SlugField(unique=True, max_length=64)
     unit = ForeignKey(Unit)
-    audio = FileField(upload_to='sounds')
+    audio = FileField(
+        max_length=128,
+        upload_to='sounds',
+        storage=OverwriteStorage()
+    )
 
     def __str__(self):
         return '{} {}'.format(self.unit.name, basename(self.audio.name))
